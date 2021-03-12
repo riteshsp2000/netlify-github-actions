d1e201865f6f3b608920adbfebb0c274cf3928d3
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-12 15:57:32 +0530

    Add eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+// let google
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React and Deploy to Netlify with GitHub Actions
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
