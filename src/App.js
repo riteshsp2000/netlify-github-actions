8c6d731458a01369038cf0ac4e0bd876cfec3230
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-15 11:42:38 +0530

    Update package lock

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React and Deploy to Netlify with GitHub Actions
         </a>
-        <p>{process.env.REACT_ENV_TRIAL}</p>
+        <p>{process.env.REACT_APP_ENV_TRIAL}</p>
       </header>
     </div>
   )
